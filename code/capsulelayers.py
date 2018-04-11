933627ffbe377cc7c8d5f7a5b90327f242d4d26e
David Huber <dhuber@berkeley.edu>
2018-04-11 00:16:02 +0000

    added model plotting

--- code/capsulelayers.py
+++ code/capsulelayers.py
@@ -157,6 +157,21 @@ class CapsuleLayer(layers.Layer):
         return tuple([None, self.num_capsule, self.dim_capsule])
 
 
+def PrimaryCap1D(inputs, dim_capsule, n_channels, kernel_size, strides, padding):
+    """
+    Added for CapsNet_for_NER project
+    
+    Apply Conv1D `n_channels` times and concatenate all capsules
+    :param inputs: 4D tensor, shape=[None, width, height, channels]
+    :param dim_capsule: the dim of the output vector of capsule
+    :param n_channels: the number of types of capsules
+    :return: output tensor, shape=[None, num_capsule, dim_capsule]
+    """
+    output = layers.Conv1D(filters=dim_capsule*n_channels, kernel_size=kernel_size, strides=strides, padding=padding,
+                           name='primarycap_conv1d')(inputs)
+    outputs = layers.Reshape(target_shape=[-1, dim_capsule], name='primarycap_reshape')(output)
+    return layers.Lambda(squash, name='primarycap_squash')(outputs)
+
 def PrimaryCap(inputs, dim_capsule, n_channels, kernel_size, strides, padding):
     """
     Apply Conv2D `n_channels` times and concatenate all capsules
@@ -165,7 +180,7 @@ def PrimaryCap(inputs, dim_capsule, n_channels, kernel_size, strides, padding):
     :param n_channels: the number of types of capsules
     :return: output tensor, shape=[None, num_capsule, dim_capsule]
     """
-    output = layers.Conv1D(filters=dim_capsule*n_channels, kernel_size=kernel_size, strides=strides, padding=padding,
+    output = layers.Conv2D(filters=dim_capsule*n_channels, kernel_size=kernel_size, strides=strides, padding=padding,
                            name='primarycap_conv2d')(inputs)
     outputs = layers.Reshape(target_shape=[-1, dim_capsule], name='primarycap_reshape')(output)
     return layers.Lambda(squash, name='primarycap_squash')(outputs)
