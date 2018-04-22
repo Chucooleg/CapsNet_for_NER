efd5ac5725eb92c7c3dbb7662e1847c5810c85af
Dave Huber <dhuber@berkeley.edu>
2018-04-22 20:33:18 +0000

    cleaning up repo

--- common/constants.py
+++
@@ -1,7 +0,0 @@
-# Constants for use by other modules.
-# This module should not import any others, except for standard library.
-
-START_TOKEN = u"<s>"
-END_TOKEN   = u"</s>"
-UNK_TOKEN   = u"<unk>"
-
